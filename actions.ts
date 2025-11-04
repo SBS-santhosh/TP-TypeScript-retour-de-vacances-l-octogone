3f53410af3d57d42fff642b020b2c2371d7a54df
user0 <user0@debian.host>
2025-11-04 09:32:52 +0100

    first

---
+++ actions.ts
@@ -0,0 +1,6 @@
+export const WARRIOR_ACTIONS: Action[] = [
+  { name: "Commit atomique", type: "attack", minDamage: 10, maxDamage: 10 },
+  ... // Compléter avec les autres actions possibles pour un guerrier
+];
+
+// Ajouter les actions pour les mages et archers
